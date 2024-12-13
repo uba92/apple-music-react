1aecaf93ecbbd061cb34ddb45a2d569b4cce4c21
Umberto Tramontano <u.tramontano92@libero.it>
2024-12-13 12:28:41 +0100

    update

--- src/components/MainNewRadio.jsx
+++ src/components/MainNewRadio.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Card } from 'react-bootstrap'
 
 const MainNewRadio = () => {
   return (
@@ -7,7 +7,66 @@ const MainNewRadio = () => {
         <h5>
           Nuovi episodi radio <i className='bi bi-chevron-right'></i>
         </h5>
-        <Col></Col>
+        <Col>
+          <Card className=' border-0'>
+            <Card.Img
+              variant='top'
+              src='src/assets/assets/images/2a.png'
+              className=' rounded-3'
+            />
+            <Card.Body>
+              <Card.Title>Prologo con Abuelo</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className=' border-0'>
+            <Card.Img
+              variant='top'
+              src='src/assets/assets/images/2b.png'
+              className=' rounded-3'
+            />
+            <Card.Body>
+              <Card.Title>The Wanderer</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className=' border-0'>
+            <Card.Img
+              variant='top'
+              src='src/assets/assets/images/2c.png'
+              className=' rounded-3'
+            />
+            <Card.Body>
+              <Card.Title>Michael Bublé & Carly Pearce</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className=' d-none d-md-block'>
+          <Card className=' border-0'>
+            <Card.Img
+              variant='top'
+              src='src/assets/assets/images/2d.png'
+              className=' rounded-3'
+            />
+            <Card.Body>
+              <Card.Title>Stephan Moccio: The Zane Lowe Interview</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className=' d-none d-lg-block'>
+          <Card className=' border-0'>
+            <Card.Img
+              variant='top'
+              src='src/assets/assets/images/2f.png'
+              className=' rounded-3'
+            />
+            <Card.Body>
+              <Card.Title>Chart Spotlight: Julia Michaels</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   )
